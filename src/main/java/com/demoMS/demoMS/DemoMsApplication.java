5046f252c99e68d592687dd71e7da66a16dc34c3
Shashank <shashank25.abap@gmail.com>
2023-08-20 17:19:21 -0400

    first commit

---
+++ src/main/java/com/demoMS/demoMS/DemoMsApplication.java
@@ -0,0 +1,13 @@
+package com.demoMS.demoMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoMS/demoMS/DemoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoMS.demoMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
